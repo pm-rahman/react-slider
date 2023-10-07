70055c076e6e182b64457111a630a48e902d22ba
pm-rahman <p.m.rahaman0181@gmail.comm>
2023-10-07 20:13:03 +0600

    first commit

---
+++ src/components/pages/home/slider/slider.js
@@ -0,0 +1,70 @@
+"use client";
+import sliderData from "@/data/sliderData";
+import Image from "next/image";
+import { useState } from "react";
+
+const Slider = () => {
+  const [currentItem, setCurrentItem] = useState(1);
+  const [prevItem, setPrevItem] = useState(0);
+  const [nextItem, setNextItem] = useState(2);
+  // prev button handler
+  const prevBtnHandler = () => {
+    if(currentItem>1){
+      setCurrentItem(prev=>prev-1);
+      setPrevItem(prev=>prev-1);
+      setNextItem(prev=>prev-1);
+    }
+  };
+  // next button handler
+  const nextBtnHandler = () => {
+    if(nextItem!==sliderData.length-1){
+      console.log(currentItem,prevItem,nextItem);
+      setCurrentItem(prev=>prev+1);
+      setPrevItem(prev=>prev+1);
+      setNextItem(prev=>prev+1);
+    }
+  };
+  return (
+    <>
+      <div className="mt-6 grid grid-cols-4 gap-2">
+        {/* prev item */}
+        <div>
+        <Image
+            src={sliderData[prevItem]?.image}
+            alt="item image"
+            className="h-[60vh] w-auto rounded"
+            width={600}
+            height={400}
+          />
+        </div>
+        {/* current item */}
+        <div className="col-span-2">
+          <Image
+            src={sliderData[currentItem]?.image}
+            alt="item image"
+            className="h-[60vh] w-auto rounded"
+            width={600}
+            height={400}
+          />
+          {/* rest design */}
+        </div>
+        {/* next item */}
+        <div>
+        <Image
+            src={sliderData[nextItem]?.image}
+            alt="item image"
+            className="h-[60vh] w-auto rounded"
+            width={600}
+            height={400}
+          />
+        </div>
+      </div>
+      <div className="flex justify-center gap-4 mt-3">
+        <span onClick={prevBtnHandler} className="border cursor-pointer px-4 py-2 rounded">Prev</span>
+        <span onClick={nextBtnHandler} className="border cursor-pointer px-4 py-2 rounded">Next</span>
+      </div>
+    </>
+  );
+};
+
+export default Slider;
---
+++ src/data/sliderData.js
@@ -0,0 +1,38 @@
+import img1 from "@/assets/image-1.jpg"
+import img2 from "@/assets/image-2.jpg"
+import img3 from "@/assets/image-3.jpg"
+import img4 from "@/assets/image-4.jpg"
+import img5 from "@/assets/image-5.jpg"
+const sliderData=[
+    {
+        id:1,
+        image:img1,
+        title:"This is title",
+        description:"This is description"
+    },
+    {
+        id:2,
+        image:img2,
+        title:"This is title",
+        description:"This is description"
+    },
+    {
+        id:3,
+        image:img3,
+        title:"This is title",
+        description:"This is description"
+    },
+    {
+        id:4,
+        image:img4,
+        title:"This is title",
+        description:"This is description"
+    },
+    {
+        id:5,
+        image:img5,
+        title:"This is title",
+        description:"This is description"
+    },
+]
+export default sliderData;
